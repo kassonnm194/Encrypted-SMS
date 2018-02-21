26dff9d0da0db50c1bfe6847333301e52f0d208b
Neil Kasson <kassonnm194@potsdam.edu>
2018-02-21 13:52:42 -0500

    Some stuff

--- Trials/AES_Expirimant.java
+++ Trials/AES_Expirimant.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class AES_Expirement {
 
-	//Fields
+	//	Fields
 	public static UUID key;
 	public static String keyString;
 	public static Scanner in;
 	public static String message;
 	public static byte[] messageBytes;
 	public static ArrayList<byte[]> blocks;
+//	public static int[] expKey;
 	public static String encoded;
 	public static String decoded;
 	
@@ -21,16 +22,54 @@ public class AES_Expirement {
 		System.out.println("\nAES test Key: "+keyString);
 		System.out.print("\nPlease enter your message: ");
 		message = in.nextLine();
-		messageBytes = message.getBytes();
-		getBlocks();
 		encode();
 		decode();
 		check();
 	}
 	
-	//	Exec: Breaks "message" string into 16 bit strings
-	//	Post: Saves 16 bit strings to "mList" field
-	public static void getBlocks() {
+		
+	public static void expandKey(){
+		
+	}
+	
+	public static void addRoundKey(int i){
+		
+	}
+	
+	public static void subBytes(int i){
+		
+	}
+	
+	public static void shiftRows(int i){
+		
+	}
+	
+	public static void mixColumns(int i){
+		
+	}
+	
+	public static void round(int i, boolean fin){
+		if(fin){
+			subBytes(i);
+			shiftRows(i);
+			addRoundKey(i);
+		} else {
+			subBytes(i);
+			shiftRows(i);
+			mixColumns(i);
+			addRoundKey(i);
+		}
+	}
+	
+	//	Exec:	Breaks string into a Byte array,
+	//			then breaks that into an ArrayList of 16-bit sub-arrays
+	//	Pre:	boolean enc (True: encode "message" || False: decode "encoded)
+	//	Post:	Saves ArrayList of blocks to "blocks" field
+	public static void getBlocks(boolean enc) {
+		if (enc)
+			messageBytes = message.getBytes();
+		else
+			messageBytes = encoded.getBytes();
 		blocks = new ArrayList<byte[]>();
 		byte[] temp = new byte[16]();
 		for (int i=0; i<messageBytes.length; i++){
@@ -48,6 +87,14 @@ public class AES_Expirement {
 	//	Exec: Encodes message using AES encryption
 	//	Post: Saves result to "encoded" field
 	public static void encode() {
+		getBlocks(True);
+		expandKey();
+		for (int i=0; i<blocks.size();
+			addRoundKey();
+			for (int j=0; j<10; i++)
+				round(i, True);
+			round(i, False);
+		}
 		
 	}
 	
@@ -64,6 +111,6 @@ public class AES_Expirement {
 	//	Exec: Decrypts encrypted message with AES
 	//	Post: Saves result to "decoded" field
 	public static void decode() {
-		
+		getBlocks(False);
 	}
 }
